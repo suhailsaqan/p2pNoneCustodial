8a00544f5c7835fb8fdb439e912cd6ab274d0a14
Suhail Saqan <suhailsaqan@users.noreply.github.com>
2022-01-29 17:33:42 -0600

    added 'status' to the contract model

--- server/models/contract.js
+++ server/models/contract.js
@@ -2,9 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const uuidv4 = require("uuid").v4;
 
+const STATUS_TYPES = {
+  CONTRACT_FUNDED_AWAITING_SETTLEMENT: "Contract funded, awaiting settlement",
+  CONTRACT_PAID_AWAITING_SETTLEMENT: "Contract paid, awaiting settlement",
+  CONTRACT_CANCELED: "Canceled",
+  CONTRACT_SETTLED: "Settled",
+  WAITING_ON_OTHER_PARTY: "Waiting on other party",
+  NEEDS_TO_PAY: "Needs to pay",
+  NEEDS_TO_SUBMIT_INVOICE: "Needs to submit invoice",
+};
+
 const contractSchema = new Schema({
   _id: { type: String, default: () => uuidv4().replace(/\-/g, "") },
   contract_name: { type: String, required: true },
+  status: {
+    type: String,
+    enum: [
+      STATUS_TYPES.CONTRACT_FUNDED_AWAITING_SETTLEMENT,
+      STATUS_TYPES.CONTRACT_PAID_AWAITING_SETTLEMENT,
+      STATUS_TYPES.CONTRACT_CANCELED,
+      STATUS_TYPES.CONTRACT_SETTLED,
+      STATUS_TYPES.WAITING_ON_OTHER_PARTY,
+      STATUS_TYPES.NEEDS_TO_PAY,
+      STATUS_TYPES.NEEDS_TO_SUBMIT_INVOICE,
+    ],
+  },
   description: { type: String, required: true },
   first_party_task: { type: String, required: true },
   first_party_amount: { type: Number, required: true },
